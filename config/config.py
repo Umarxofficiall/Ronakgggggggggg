b50033e7245820c5a62411b4d9375ccb9770272d
Umarxofficiall <ig5843089@gmail.com>
2025-02-17 16:19:50 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -5,23 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "9972455"
+API_ID = ""
 # -------------------------------------------------------------
-API_HASH = "5b1fd83b698e4e6670f3dcb053eecc06"
+API_HASH = ""
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7699442900:AAG-G6K1J_ExrCnAYAw6MHsSjb224jMJo28")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 STRING1 = getenv("STRING_SESSION", None)
 DB_NAME = "ronakDB"
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Aigf:AiGFChatbot@cluster0.sqwys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = int(getenv("OWNER_ID", "6170050819"))
-BOT_ID = int(getenv("BOT_ID", "7699442900"))
-SUPPORT_GRP = "Spark_Developer_Bots"
-UPDATE_CHNL = "Spark_Developer_Bots"
-OWNER_USERNAME = "Spark_Developer"
+MONGO_URL = getenv("MONGO_URL", "")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+BOT_ID = int(getenv("BOT_ID", ""))
+SUPPORT_GRP = "" #username
+UPDATE_CHNL = ""#username
+OWNER_USERNAME = ""#username
 TIME_ZONE = "Asia/Kolkata"
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002333236341"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "")
 # --------------------------------------------------------------
-SUDOERS = list(map(int, getenv("SUDOERS", "7707866111").split()))
+SUDOERS = list(map(int, getenv("SUDOERS", "").split()))
 # --------------------------------------------------------------
 
 ### DONT TOUCH or EDIT codes after this line
@@ -30,6 +30,6 @@ BANNED_USERS = filters.user()
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Badhacker98/ShizuChat_Bot",
+    "https://github.com/Umarxofficiall/Ronakgggggggggg",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
