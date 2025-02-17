291a0f0eadcd5032dbf1a2fecf86205a4088474d
Umar565 <titaniumgamingr@gmail.com>
2025-02-17 15:49:49 +0530

    Update storage.py

--- shizuchat/database/storage.py
+++ shizuchat/database/storage.py
@@ -2,10 +2,9 @@ import random
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 CHAT_STORAGE = [
-    "mongodb+srv://Musicbot:AdvMusicBot2@cluster0.qsj1x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    "mongodb+srv://Musicbot:AdvMusicBot2@cluster0.qsj1x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+"mongodb+srv://chatbot1:a@cluster0.pxbu0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/",
 ]
-
 BADMUNDA = MongoCli(random.choice(CHAT_STORAGE))
 chatdb = BADMUNDA.Anonymous
 chatai = chatdb.Word.WordDb
